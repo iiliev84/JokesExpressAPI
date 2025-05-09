e5ae803ace9039cb06f4f8a7c4c8ef811ddebcc9
ILIAN ILIEV <iiliev84@gmail.com>
2025-05-08 21:30:54 -0500

    working server

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 import express from "express"
-import jokes from "./jokes"
+import jokes from "./jokes.js"
 const app = express()
 
+app.route('/jokes/random').get((req,res)=>{
+    const random = Math.floor(Math.random() * jokes.length)
+    res.send(jokes[random])
+})
+
+app.route('/jokes/:id').get((req,res)=>{
+    const id = Number(req.params.id)
+    const found = jokes.find(joke => joke.id == id)
+    if(found){
+        res.send(found)
+    }else{
+        res.status(404).send("Joke not found!")
+    }
+})
+
+app.route('/jokes').get((req,res)=>{
+    res.send(jokes)
+})
+
+app.route('/').get((req,res)=>{
+    res.send("Are you ready to laugh?")
+})
 
 export default app
